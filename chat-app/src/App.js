90771d20ee8744582514a649dc229777a93547c9
*Lighty7 <yashwanthyash7601@gmail.com>
2024-02-04 23:06:26 +0530

    ain

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { io } from "socket.io-client";
 import './App.css';
 
-function App() {
+const socket = io('<http://localhost:3000>');
+
+function ChatRoom() {
+  const [messages, setMessages] = useState([]);
+  const [messageText, setMessageText] = useState('');
+  const [user, setUser] = useState(null);
+
+  // Join a chat room
+  const joinChatRoom = (userDetails) => {
+    setUser(userDetails);
+    socket.emit('join', userDetails);
+  };
+
+  // Leave a chat room
+  const leaveChatRoom = () => {
+    socket.emit('leave', user);
+    setUser(null);
+  };
+
+  // More code will go here
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="chat-room">
+      // The chat room components will go here
     </div>
   );
 }
 
-export default App;
+export default ChatRoom;
---
+++ chat-app/src/ChatRoom.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function ChatRoom() {
+  return (
+    <div className="chat-room">
+      // The chat room components will go here
+    </div>
+  );
+}
+export default ChatRoom;
