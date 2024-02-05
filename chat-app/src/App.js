a3925f252b8aa0bfcd2e07122d492bec354315f7
*Lighty7 <yashwanthyash7601@gmail.com>
2024-02-05 22:30:18 +0530

    ain

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { io } from "socket.io-client";
 import './App.css';
-
+//sleep u have exams tmorrow
 const socket = io('<http://localhost:3000>');
 
 function ChatRoom() {
